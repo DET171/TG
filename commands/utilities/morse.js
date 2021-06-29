220b5f52b507abdf1aa7875547d68369eb5d6be1
DET171 <mrgeek484@gmail.com>
2021-06-29 18:55:26 +0800

    woo hoo

--- commands/utilities/morse.js
+++ commands/utilities/morse.js
@@ -1,6 +1,9 @@
 const { Command } = require('yuuko');
 module.exports = new Command('morse', (message, args) => {
 	let str = args.slice(1).join(' ');
+	str = ` ${str}`;
+	str = str.toLowerCase().replace(/\W/, '');
+	console.log(str);
 	const morseCode = {
 		'A': '.-',
 		'B': '-...',
@@ -27,17 +30,66 @@ module.exports = new Command('morse', (message, args) => {
 		'X': '-..-',
 		'Y': '-.--',
 		'Z': '--..',
+		' ': '==',
+	};
+
+	var ref = {
+		'.-':     'a',
+		'-...':   'b',
+		'-.-.':   'c',
+		'-..':    'd',
+		'.':      'e',
+		'..-.':   'f',
+		'--.':    'g',
+		'....':   'h',
+		'..':     'i',
+		'.---':   'j',
+		'-.-':    'k',
+		'.-..':   'l',
+		'--':     'm',
+		'-.':     'n',
+		'---':    'o',
+		'.--.':   'p',
+		'--.-':   'q',
+		'.-.':    'r',
+		'...':    's',
+		'-':      't',
+		'..-':    'u',
+		'...-':   'v',
+		'.--':    'w',
+		'-..-':   'x',
+		'-.--':   'y',
+		'--..':   'z',
+		'.----':  '1',
+		'..---':  '2',
+		'...--':  '3',
+		'....-':  '4',
+		'.....':  '5',
+		'-....':  '6',
+		'--...':  '7',
+		'---..':  '8',
+		'----.':  '9',
+		'-----':  '0',
+		'==': ' ',
+	};
+	const convertToMorse = (string) => {
+		return string.toUpperCase().split('').map(el => {
+			return morseCode[el] ? morseCode[el] : el;
+		}).join(' ');
+	};
+	const convertToStr = (string) => {
+		return string.toUpperCase().split(' ').map(el => {
+			return ref[el] ? ref[el] : el;
+		}).join('');
 	};
 	if(!args.length) {
 		message.channel.createMessage(`<@${message.author.id}>, you missed out the following arguments! \n e.g. \`${process.env.PREFIX} <encode/decode> <string>\``);
 	}
 	if(args[0] == 'encode') {
-		str = str.toLowerCase().replace(/[^a-z]/g, '');
-		const convertToMorse = (string) => {
-			return string.toUpperCase().split('').map(el => {
-				return morseCode[el] ? morseCode[el] : el;
-			}).join(' ');
-		};
 		message.channel.createMessage(convertToMorse(str));
+		console.log(convertToStr(convertToMorse(str)));
+	}
+	if(args[0] == 'decode') {
+		message.channel.createMessage(convertToStr(str));
 	}
 });
