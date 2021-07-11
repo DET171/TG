38720e79433c7a5557788b1549e4eb8b43c135ed
DET171 <mrgeek484@gmail.com>
2021-07-11 14:59:31 +0800

    added roll command

---
+++ commands/fun/roll.js
@@ -0,0 +1,40 @@
+const { Command } = require('yuuko');
+require('dotenv').config();
+module.exports = new Command(['roll', 'rolladie', 'rolladice'], (message, args) => {
+	const arg = args.join(' ');
+	if(!args.length) {
+		message.channel.createMessage({
+			embed: {
+				title: `${message.author.username} rolled a die!`,
+				description: `${message.author.mention} rolled a die and got **${Math.floor(Math.random() * 6) + 1}**!`,
+				color: 12252021,
+				fields: [
+				],
+			},
+		});
+	}
+	else {
+		try {
+			const num = arg.trim().split('d');
+			const times = parseInt(num[0]);
+			const max = parseInt(num[1]);
+			const nums = [];
+			for(let i = 0; i < times; i++) {
+				let result = Math.floor(Math.random() * max); // eslint-disable-line prefer-const
+				result = result + 1;
+				nums.push(result);
+			}
+			message.channel.createMessage({
+				embed: {
+					title: `${message.author.username} rolled a ${times} dice!`,
+					description: `${message.author.mention} rolled a ${times} dice and got [ **${nums.join(' ')}** ]!`,
+					color: 12252021,
+				},
+			});
+		}
+		catch(err) {
+			console.warn(err);
+			message.channel.createMessage(`${message.author.mention}, the correct usage would be \`${process.env.PREFIX} roll <number of dice to roll>d<highest number on the die>\``);
+		}
+	}
+});
--- commands/info/guild.js
+++ commands/info/guild.js
@@ -3,17 +3,29 @@ const { today } = require('../../utils.js');
 const moment = require('moment');
 module.exports = new Command(['guild', 'server'], (message) => {
 	const guild = message.channel.guild;
+	const owner = guild.members.get(guild.ownerID);
 	message.channel.createMessage({
 		embed: {
 			title: 'Guild Information',
+			description: `Guild information for ${guild.name} (id: \`${guild.id}\`)`,
 			color: 11272041,
 			thumbnail: {
 				url: guild.iconURL,
 			},
 			fields: [
 				{
+					name: 'Owner:',
+					value: `${owner.username}#${owner.discriminator} (id: \`${guild.ownerID}\`)`,
+					inline: false,
+				},
+				{
 					name: 'Created at:',
 					value: `${moment.utc(guild.createdAt).format('MMMM, Do YYYY, h:mm:ss a')}`,
+					inline: false,
+				},
+				{
+					name: 'Member count:',
+					value: `${guild.memberCount} members`,
 					inline: false,
 				},
 			],
